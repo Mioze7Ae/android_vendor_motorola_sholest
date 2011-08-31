9ffa333d8b8fb0737b64ea3b2c68512b465125d0
Mioze7Ae <Mioze7Ae@gmail.com>
2011-08-31 13:04:01 -0400

    Use compiled Usb.apk
    Change-Id: I059cf690077d37e61ee8ce9c71ba82a95674e47c

--- sholest-vendor-blobs.mk
+++ sholest-vendor-blobs.mk
@@ -262,8 +262,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sholest/proprietary/app/ProgramMenuSystem.apk:/system/app/ProgramMenuSystem.apk \
     vendor/motorola/sholest/proprietary/app/PersonalPortal.apk:/system/app/PersonalPortal.apk \
     vendor/motorola/sholest/proprietary/app/QuickOffice.apk:/system/app/QuickOffice.apk \
-    vendor/motorola/sholest/proprietary/app/Usb.apk:/system/app/Usb.apk \
     vendor/motorola/sholest/proprietary/app/Mynet.apk:/system/app/Mynet.apk
+#    vendor/motorola/sholest/proprietary/app/Usb.apk:/system/app/Usb.apk \
 
 PRODUCT_COPY_FILES += \
     vendor/motorola/sholest/proprietary/app/KoreanIME.apk:/system/app/KoreanIME.apk \
